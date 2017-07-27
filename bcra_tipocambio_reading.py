caf31d531f7877a75eb7f784561c4c7ff2658371
fabianvarisco <30510128+fabianvarisco@users.noreply.github.com>
2017-07-27 18:35:29 -0300

    Create bcra_tipocambio_reading.py

---
+++ bcra_tipocambio_reading.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+import csv
+date = "11/07/2016" 
+res = requests.get("http://www.bcra.gob.ar/PublicacionesEstadisticas/Cotizaciones_por_fecha_2.asp?date2=" + date)
+soup = BeautifulSoup(res.content,'lxml')
+table = soup.find_all('table')[0] 
+df = pd.read_html(str(table), thousands=".", decimal=",", skiprows={0,1})[0].applymap(lambda x : "" if type(x) == str and len(x) == x.count("-") else x)
+df.columns = df.columns.droplevel()
+df.columns = ['MONEDA', 'DOLARES', 'PESOS_ARG']
+df['MONEDA'] = df['MONEDA'].map(lambda x: x.upper())
+df.to_csv("bcra_cotizaciones_por_fecha_"+str.replace(date, "/", "-")+".csv", index=False, quoting=csv.QUOTE_NONNUMERIC)
+df
