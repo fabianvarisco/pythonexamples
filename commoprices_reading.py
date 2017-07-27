bf4ee79e64d89e6336424eb300259ce1910edeaf
fabianvarisco <30510128+fabianvarisco@users.noreply.github.com>
2017-07-27 18:31:27 -0300

    Update commoprices_reading.py

--- commoprices_reading.py
+++ commoprices_reading.py
@@ -1,5 +1,6 @@
 import requests
-token="wsg0pK60DVpUUv5MFSamBy5Q0o5GWKwZYZ5t5XUuK4h2fcrKTHAdEK7TeHjL"
+token="user access token from https://api.commoprices.com"
+#like this: token="wsg0pK60DVpUUv5MFSamBy5Q0o5GWKwZYZ5t5XUuK4h2..."
 
 headers = {'authorization': "Bearer " + token, 'accept': "application/json" }
 res = requests.get("https://api.commoprices.com/v1/imf", headers=headers)
@@ -7,3 +8,7 @@ res.status_code
 df=pd.DataFrame(res.json()["data"])
 df
 
+res = requests.get("https://api.commoprices.com/v1/imf/POILDUB/data", headers=headers)
+res.status_code
+df=pd.DataFrame(res.json()["data"])
+df
