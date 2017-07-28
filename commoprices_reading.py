78679b819c85bb470aef7d9339c17f0460b8553c
fabianvarisco <30510128+fabianvarisco@users.noreply.github.com>
2017-07-28 10:18:05 -0300

    Update commoprices_reading.py

--- commoprices_reading.py
+++ commoprices_reading.py
@@ -1,6 +1,8 @@
 import requests
 import pandas as pd
-token="wsg0pK60DVpUUv5MFSamBy5Q0o5GWKwZYZ5t5XUuK4h2fcrKTHAdEK7TeHjL"
+import csv
+import json
+token="wsg0pK60DVpUUv5MFSamBy5Q0o5GWKwZYZ5t5XUuK4h2..."
 #like this: token="wsg0pK60DVpUUv5MFSamBy5Q0o5GWKwZYZ5t5XUuK4h2..."
 
 headers = {'authorization': "Bearer " + token, 'accept': "application/json" }
@@ -8,10 +10,13 @@ res = requests.get("https://api.commoprices.com/v1/imf", headers=headers)
 res.status_code
 j = res.json()["data"]
 df=pd.DataFrame(j)
+df.to_csv("api_commoprices_com_v1_imf.csv", index=False, quoting=csv.QUOTE_NONNUMERIC)
 df
 
-res = requests.get("https://api.commoprices.com/v1/imf/POILDUB/data", headers=headers)
-res.status_code
-j = res.json()["data"]
-df=pd.DataFrame(j)
-df
+dbname="PSMEA"
+res = requests.get("https://api.commoprices.com/v1/imf/"+ dbname + "/data", headers=headers)
+df=pd.DataFrame(res.json()["data"])
+ds = df.loc["dataseries","request"]
+df2 = pd.DataFrame(ds, columns=["DAY", "VALUE"])
+df2.to_csv("api_commoprices_com_v1_imf_" + dbname + ".csv", index=False, quoting=csv.QUOTE_NONNUMERIC)
+df2
