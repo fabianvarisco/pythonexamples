558f941414cdcdd64a084f6234e1565e5d28cd21
fabianvarisco <30510128+fabianvarisco@users.noreply.github.com>
2017-08-03 10:30:46 -0300

    Update bcra_tipocambio_reading.py

--- bcra_tipocambio_reading.py
+++ bcra_tipocambio_reading.py
@@ -7,7 +7,6 @@ res = requests.get("http://www.bcra.gob.ar/PublicacionesEstadisticas/Cotizacione
 soup = BeautifulSoup(res.content,'lxml')
 table = soup.find_all('table')[0] 
 df = pd.read_html(str(table), thousands=".", decimal=",", skiprows={0,1})[0].applymap(lambda x : "" if type(x) == str and len(x) == x.count("-") else x)
-df.columns = df.columns.droplevel()
 df.columns = ['MONEDA', 'DOLARES', 'PESOS_ARG']
 df['MONEDA'] = df['MONEDA'].map(lambda x: x.upper())
 df.sort_values(by="MONEDA", inplace=True)
