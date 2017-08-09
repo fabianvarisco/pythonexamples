796e691031aa0d7ce500319cffb63b3c09630f5e
fabianvarisco <30510128+fabianvarisco@users.noreply.github.com>
2017-08-09 14:54:46 -0300

    Update bcra_tipocambio_reading.py

--- bcra_tipocambio_reading.py
+++ bcra_tipocambio_reading.py
@@ -8,7 +8,7 @@ soup = BeautifulSoup(res.content,'lxml')
 table = soup.find_all('table')[0] 
 df = pd.read_html(str(table), thousands=".", decimal=",", skiprows={0,1})[0].applymap(lambda x : "" if type(x) == str and len(x) == x.count("-") else x)
 df.columns = ['MONEDA', 'DOLARES', 'PESOS_ARG']
-df['MONEDA'] = df['MONEDA'].map(lambda x: x.upper())
+df['MONEDA'] = df['MONEDA'].str.upper()
 df.sort_values(by="MONEDA", inplace=True)
 df.to_csv("bcra_cotizaciones_por_fecha_"+str.replace(date, "/", "-")+".csv", index=False, quoting=csv.QUOTE_NONNUMERIC)
 df
